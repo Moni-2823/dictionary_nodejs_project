ae524f3938fa6f6a147773b69466cfe81e4b1763
RAM YADAV <moniyadav2823@gmail.com>
2021-04-30 17:58:46 +0530

    mongodb atlas password added

--- database/server.js
+++ database/server.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://moniyadav:<password>@cluster0.hcpdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://moniyadav:yadavmoni0987@cluster0.hcpdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 console.log('connected to mongoose');
 
 module.exports = { mongoose };
